a3f068ea6ca1e601955609559c19986a642f4747
Tim Kye <Timothy.Kye@nike.com>
2016-07-05 15:16:26 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,196 @@
+var request = require('request-micro')
+var urlJoin = require('url-join')
+
+module.exports = cerberus
+
+var cerberusVersion = 'v1'
+var ec2MetadataUrl = 'http://169.254.169.254/latest/meta-data/iam/info'
+var ec2InstanceDataUrl = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
+
+function log () { console.log.apply(console, ['cerberus-node'].concat(Array.prototype.slice.call(arguments))) }
+function noop () { }
+
+function shallowCopy (target, source) {
+  for (var key in source) {
+    if (source.hasOwnProperty(key)) {
+      target[key] = source[key]
+    }
+  }
+  return target
+}
+
+function cerberus (options) {
+  options = shallowCopy({}, options)
+  // Override options with env variables
+  if (process.env.CERBERUS_TOKEN) {
+    options.token = process.env.CERBERUS_TOKEN
+  }
+  if (process.env.CERBERUS_ADDR) {
+    options.hostUrl = process.env.CERBERUS_ADDR
+  }
+  // Validate options
+  if (!options || typeof options !== 'object') {
+    throw new Error('options paramater is required')
+  }
+  if (!options.aws || typeof options.aws !== 'object') {
+    throw new Error('options.aws parameter is required')
+  }
+  if (typeof options.hostUrl !== 'string') {
+    throw new Error('options.hostUrl must be a URL string')
+  }
+
+  options.log = options.debug ? log : noop
+
+  return {
+    get: callCerberus.bind(null, 'GET', options),
+    set: callCerberus.bind(null, 'POST', options),
+    list: callCerberus.bind(null, 'LIST', options),
+    delete: callCerberus.bind(null, 'DELETE', options)
+  }
+}
+
+function callCerberus (type, options, keyPath, cb) {
+  getToken(options, function (err, authToken) {
+    if (err) return cb(err)
+    request({
+      method: type === 'LIST' ? 'GET' : type,
+      url: urlJoin(options.hostUrl, cerberusVersion, 'secret', keyPath) + (type === 'LIST' ? '?list=true' : ''),
+      headers: { 'X-Vault-Token': authToken },
+      json: true
+    }, function (err, res, data) {
+      if (err) return cb(err)
+      else return cb(null, data.data)
+    })
+  })
+}
+
+function getToken (options, cb) {
+  if (options.tokenExpiresAt && (options.tokenExpiresAt <= Date.now())) {
+    options.tokenExpiresAt = null
+    options.token = null
+  }
+
+  // Already has token
+  if (options.token) return cb(null, options.token)
+
+  // Use Lambda
+  if (options.lambdaContext) {
+    return getLambdaMetadata(options, makeMetadataResponseHandler(options, cb))
+  }
+
+  // Default to EC2
+  return getEc2Metadata(options, makeMetadataResponseHandler(options, cb))
+}
+
+function makeMetadataResponseHandler (options, cb) {
+  return function (err, metadata) {
+    if (err) return cb(err)
+    if (options.aws.config.region === undefined) {
+      options.aws.config.region = metadata.region
+    }
+    return auth(options, metadata.accountId, metadata.roleName, metadata.region, cb)
+  }
+}
+
+function auth (options, accountId, roleName, region, cb) {
+  var authOptions = {
+    url: urlJoin(options.hostUrl, cerberusVersion, '/auth/iam-role'),
+    body: { 'account_id': accountId, 'role_name': roleName, 'region': region },
+    json: true
+  }
+  request.post(authOptions, function (err, res, authResult) {
+    if (err) return cb(err)
+    decryptAuthResult(options, authResult, function (err, token) {
+      if (err) return cb(err)
+      console.log('auth result', token)
+      // Expire 10 seconds before lease is up, to account for latency
+      options.tokenExpiresAt = Date.now + token['lease_duration'] - 600
+      options.token = token['client_token']
+      cb(null, options.token)
+    })
+  })
+}
+
+function decryptAuthResult (options, authResult, cb) {
+  console.log('decrypting', authResult)
+  if (!authResult['auth_data']) {
+    return cb(new Error('cannot decrypt token, auth_data is missing'))
+  }
+  var text = new Buffer(authResult['auth_data'], 'base64')
+  console.log('config', options.aws.config)
+  // console.log('aws', options.aws)
+  var kms = new options.aws.KMS({ apiVersion: '2014-11-01' })
+
+  kms.decrypt({ CiphertextBlob: text }, function (err, kmsResult) {
+    console.log('kms result', kmsResult)
+    if (err) {
+      return cb(!isKmsAccessError(err)
+        ? err
+        : new Error('You do not have access to the KMS key required for authentication. The most likely cause is that your IAM role does not have the KMS Decrypt action. You will need to add it to your role.'))
+    }
+    var token
+
+    try {
+      token = JSON.parse(new Buffer(kmsResult.Plaintext).toString())
+    } catch (e) {
+      cb(new Error('Error parsing KMS decrypt Result. ' + e.message))
+      return
+    }
+    cb(null, token)
+  })
+}
+
+function isKmsAccessError (error) {
+  return error.message && error.message.indexOf('The ciphertext references a key that either does not exist or you do not have access to') !== -1
+}
+
+function getEc2Metadata (options, cb) {
+  var iam = new options.aws.IAM({apiVersion: '2010-05-08'})
+  var metadata = { }
+
+  request({ url: ec2MetadataUrl, json: true }, function (err, result, data) {
+    if (err) return cb(err)
+    if (data.Code !== 'Success') return cb(data)
+
+    var arn = data.InstanceProfileArn.split(':')
+    var instanceProfileName = arn[5].substring(arn[5].indexOf('/') + 1)
+    metadata.accountId = arn[4]
+
+    iam.getInstanceProfile({ InstanceProfileName: instanceProfileName }, function (err, data) {
+      if (err) return cb(err)
+      var roles = data.InstanceProfile.Roles
+      if (!roles.length) return cb(new Error('No roles returned for instance profile ' + instanceProfileName))
+
+      metadata.roleName = roles[0].RoleName
+
+      request({ url: ec2InstanceDataUrl, json: true }, function (err, result, data) {
+        if (err) return cb(err)
+        metadata.region = data.region
+        options.log('metadata', metadata)
+        cb(null, metadata)
+      })
+    })
+  })
+}
+
+function getLambdaMetadata (options, cb) {
+  var lambda = new options.aws.Lambda({ apiVersion: '2015-03-31' })
+  var arn = options.lambdaContext.invokedFunctionArn.split(':')
+
+  var metadata = { region: arn[3], accountId: arn[4] }
+  var params = { FunctionName: arn[6], Qualifier: arn[7] }
+
+  lambda.getFunctionConfiguration(params, function (err, data) {
+    if (err) {
+      console.log('error getting metadata', err, err.stack)
+      cb(err)
+      return
+    }
+
+    metadata.roleName = data.Role.split('/')[1]
+
+    console.log('retrieved metadata values', metadata)
+
+    cb(null, metadata)
+  })
+}
