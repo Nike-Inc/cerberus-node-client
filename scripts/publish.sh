04baf2bc69fb8764f37401e9b0dfc9236ebcc06c
Justin Field <fieldju@gmail.com>
2018-12-21 12:52:15 -0800

    remove tutorials from jsdocs as there are non and only run npm release once (#21)

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+echo "Preparing .npmrc"
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
+echo 'registry=http://registry.npmjs.org' >> .npmrc
+
+npm publish --otp-token=$(oathtool -b --totp ${NPM_OTP})
